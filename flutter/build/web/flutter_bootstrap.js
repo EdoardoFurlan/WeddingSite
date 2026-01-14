197b2ad48866d375284dcf3979e84b910169bb7f
Edoardo Furlan <edoardofurlan23@gmail.com>
2026-01-14 23:45:15 +0100

    api

--- flutter/build/web/flutter_bootstrap.js
+++ flutter/build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "288916357"
+    serviceWorkerVersion: "2020308597"
   }
 });
