b2c12d33f53f8cadd6a84e77f04a5208c84a36cf
Laura Gagliazzo <lauragag19@gmail.com>
2026-01-17 00:01:44 +0100

    Aggiornata ui - elveted buttons

--- flutter/build/web/flutter_bootstrap.js
+++ flutter/build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3977592100"
+    serviceWorkerVersion: "2826349992"
   }
 });
