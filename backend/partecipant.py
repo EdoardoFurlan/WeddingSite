42c2fc4a4b3103a71010fedd3c20263dfb2e3c01
Edoardo Furlan <edoardofurlan23@gmail.com>
2026-01-19 21:19:55 +0100

    aggiunta data

--- backend/partecipant.py
+++ backend/partecipant.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional
 
 # Definizione Modello Pydantic (per validazione dati API)
 class Partecipant(BaseModel):
