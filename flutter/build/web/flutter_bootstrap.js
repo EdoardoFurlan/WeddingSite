50de7d6bbd055c262a733a96fe5939137c02e53a
Laura Gagliazzo <lauragag19@gmail.com>
2026-02-09 20:47:01 +0100

    Aggiornato link tappeto

--- flutter/build/web/flutter_bootstrap.js
+++ flutter/build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4133788215"
+    serviceWorkerVersion: "1519226998"
   }
 });
